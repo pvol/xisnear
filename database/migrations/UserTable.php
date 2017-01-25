1bc8254b685ee38782f68b44fd6080dbbbe83496
xisnear <service@xisnear.com>
2017-01-25 22:21:19 +0800

    add permission tables

---
+++ database/migrations/UserTable.php
@@ -0,0 +1,25 @@
+<?php
+
+use Xisnear\Frame\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class UserTable extends Migration{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        $this->table('x_users', function(Blueprint $table) {
+            $table->create();
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('display_name', 100);
+            $table->string('email', 100);
+            $table->string('password', 100);
+            $table->string('remember_token', 100);
+            $table->timestamps();
+        });
+    }
+}
